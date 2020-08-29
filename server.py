67984892545a5e2ea339345ba430f3575d54acd6
beqakd <bgoga16@freeuni.edu.ge>
2020-08-29 19:38:37 +0400

    implement websocket signaling

--- server.py
+++ server.py
@@ -1,74 +1,23 @@
 import asyncio
-import os
+import websockets
 import json
-from aiohttp import web
-import sys
-
 from server.ConnectionContainer import ConnectionContainer
 
-
-ROOT = os.path.dirname(__file__)
-routes = web.RouteTableDef()
-
+# Create container
 connection_container = ConnectionContainer()
 
-import aiortc
+# WebSocket server
+async def server(websocket, path):
+    # Take request parse and send message back.
+    async for message in websocket:
+        params = json.loads(message)
+
+        # Taking video streaming sdp
+        answer = await connection_container.handle_offer(sdp=params["sdp"], mode='cartoon')
+        await websocket.send(answer.sdp)
 
 
-def aiortc_multiple_stream_patch() -> None:
-    def _patched_route_rtp(self, packet):
-        ssrc_receiver = self.ssrc_table.get(packet.ssrc)
-
-        # the SSRC are known
-        if ssrc_receiver is not None:
-            return ssrc_receiver
-
-        pt_receiver = self.payload_type_table.get(packet.payload_type)
-
-        # the SSRC is unknown but the payload type matches, update the SSRC table
-        if ssrc_receiver is None and pt_receiver is not None:
-            self.ssrc_table[packet.ssrc] = pt_receiver
-            return pt_receiver
-
-        # discard the packet
-        return None
-
-    aiortc.rtcdtlstransport.RtpRouter.route_rtp = _patched_route_rtp
-
-
-aiortc_multiple_stream_patch()
-
-
-async def on_shutdown():
-    pass
-
-
-@routes.get("/")
-async def index(request):
-    content = open(os.path.join(ROOT, "server/public/index.html"), "r").read()
-    return web.Response(content_type="text/html", text=content)
-
-
-@routes.post("/offer")
-async def offer(request):
-    params = await request.json()
-
-    answer = await connection_container.handle_offer(
-        sdp=params["sdp"], mode=params["mode"]
-    )
-    print(params["mode"])
-
-    return web.Response(
-        content_type="application/json",
-        body=json.dumps({"sdp": answer.sdp, "type": "answer"}),
-    )
-
-
-if __name__ == "__main__":
-    app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-
-    app.add_routes(routes)
-    app.add_routes([web.static("/", ROOT + "server/public")])
-    port = 3000 if len(sys.argv) == 1 else sys.argv[1]
-    web.run_app(app, port=port)
+# Start websocket server
+start_server = websockets.serve(server, "localhost", 5000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ server_old.py
@@ -0,0 +1,74 @@
+import asyncio
+import os
+import json
+from aiohttp import web
+import sys
+
+from server.ConnectionContainer import ConnectionContainer
+
+
+ROOT = os.path.dirname(__file__)
+routes = web.RouteTableDef()
+
+connection_container = ConnectionContainer()
+
+import aiortc
+
+
+def aiortc_multiple_stream_patch() -> None:
+    def _patched_route_rtp(self, packet):
+        ssrc_receiver = self.ssrc_table.get(packet.ssrc)
+
+        # the SSRC are known
+        if ssrc_receiver is not None:
+            return ssrc_receiver
+
+        pt_receiver = self.payload_type_table.get(packet.payload_type)
+
+        # the SSRC is unknown but the payload type matches, update the SSRC table
+        if ssrc_receiver is None and pt_receiver is not None:
+            self.ssrc_table[packet.ssrc] = pt_receiver
+            return pt_receiver
+
+        # discard the packet
+        return None
+
+    aiortc.rtcdtlstransport.RtpRouter.route_rtp = _patched_route_rtp
+
+
+aiortc_multiple_stream_patch()
+
+
+async def on_shutdown():
+    pass
+
+
+@routes.get("/")
+async def index(request):
+    content = open(os.path.join(ROOT, "server/public/index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+@routes.post("/offer")
+async def offer(request):
+    params = await request.json()
+
+    answer = await connection_container.handle_offer(
+        sdp=params["sdp"], mode=params["mode"]
+    )
+    print(params["mode"])
+
+    return web.Response(
+        content_type="application/json",
+        body=json.dumps({"sdp": answer.sdp, "type": "answer"}),
+    )
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+
+    app.add_routes(routes)
+    app.add_routes([web.static("/", ROOT + "server/public")])
+    port = 3000 if len(sys.argv) == 1 else sys.argv[1]
+    web.run_app(app, port=port)
