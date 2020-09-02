797903a4cfeb7fdb08665dec9e20512cb2e27584
bnMisho <m.nazgaidze@omedia.ge>
2020-09-02 15:41:04 +0400

    add app.py

--- app.py
+++ app.py
@@ -1,26 +1,55 @@
-from PIL import Image
-import numpy as np
+import argparse
+import tensorflow as tf
 import cv2
+import os
 
-from human.detect import cocoDetection as human_detector
-from mask.detect import inference as detect_masks
+from view.mask_camera import run_mask_camera
+from view.track_camera import run_track_camera
+from mask.detect import inference
+from DB.db import initialize_database
+from config import config_import as conf
 
+VIDEO_PATHS = conf.get_config_data_by_key('video_paths')
+CAMERA_IN_VIDEO_PATH = VIDEO_PATHS['CAMERA_IN']
+CAMERA_OUT_VIDEO_PATH = VIDEO_PATHS['CAMERA_OUT']
 
-def show_people(img_path):
-    detector = human_detector()
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    boxes = detector.inference(img, draw=True)
-    Image.fromarray(img).show()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Face Mask Detection")
+    parser.add_argument('--video-mode', type=int, default=0,
+                        help='run mask_camera or track_camera,\
+                             `0` means mask_camera')
+    parser.add_argument('--output', type=str, default='0',
+                        help='save the result to a new video, \
+                            `0` means dont save')
 
+    args = parser.parse_args()
 
-def show_masks(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    detect_masks(img, show_result=True)
+    # for testing config via gui
+    try:
+        CAMERA_IN_VIDEO_PATH = int(CAMERA_IN_VIDEO_PATH)
+        CAMERA_OUT_VIDEO_PATH = int(CAMERA_OUT_VIDEO_PATH)
+    except ValueError:
+        pass
 
+    initialize_database()
+    output_dir = ''
 
-# show_masks('')
-show_people(
-    "/home/misho/Documents/117638566_2844415205830788_4375767431198241446_n.jpg"
-)
+    if args.output == '1':
+        output_dir = './out_data/'
+        try:
+            os.mkdir(output_dir)
+        except: 
+            pass
+        output_dir += 'test.avi'
+
+    # Run the in camera in bidirectional mode
+    # Tracking is set to False since the track camera
+    # performs this task
+    if args.video_mode:
+        run_track_camera(CAMERA_OUT_VIDEO_PATH, output_dir, conf_thresh=0.5, 
+                        target_shape=(260, 260), debug=True)
+
+    # Run the out camera in bidirectional mode
+    else:
+        run_mask_camera(CAMERA_IN_VIDEO_PATH, output_dir, conf_thresh=0.5, 
+                        target_shape=(260, 260))
